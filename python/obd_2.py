c51fb018f3a1506e7cd052c7bc75ef3bb9aaf6c7
kimgwangho <pti1409@naver.com>
2018-06-24 11:41:55 +0900

    Update obd_2.py

--- python/obd_2.py
+++ python/obd_2.py
@@ -1,4 +1,5 @@
 import bluetooth
+import socket
 
 def file_read():
     
@@ -16,31 +17,47 @@ def file_read():
 
 
 server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
- 
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 port = 1
 server_socket.bind(("",port))
 server_socket.listen(1)
- 
+
 client_socket,address = server_socket.accept()
 print ("Accepted connection from " + address[0])
 while 1:
- 
-    data = client_socket.recv(1024)
- 
-    data = data.decode("utf8").strip()
- 
-    print ("Received: " + data)
 
-  
-    if (data == "1"):    
+    try : 
+        data = client_socket.recv(1024)
+     
+        data = data.decode("utf8").strip()
+     
+        print ("Received: " + data)
 
-        obd_data = file_read()
-        client_socket.send( obd_data + '\n')
-  
-  
-    if (data == "q"):
-        print ("Quit")
-        break
-    
- #client_socket.close()
+      
+        if (data == "1"):    
+
+            obd_data = file_read()
+            client_socket.send( obd_data + '\n')
+      
+      
+        if (data == "q"):
+            print ("Quit")
+            break
+        
+    except Exception as ex:
+         #client_socket.close()
+        server_socket.close()
+        print("socket close")
+
+        server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        port = 1
+        server_socket.bind(("",port))
+        server_socket.listen(1)
+
+        client_socket,address = server_socket.accept()
+
+        
 server_socket.close()
